b5576611033c282ddf7769c0ca894deb2f2154d3
griffinodow <griffin@griffindow.com>
2022-02-25 07:36:05 +0000

    Refactor index with imported generateCreature function

--- index.js
+++ index.js
@@ -1,41 +1,17 @@
-import { name } from "./names.js";
-import { personalityTrait } from "./personalityTrait.js";
+import { generateCreature } from "./modules/generators.js";
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
+/**
+ * Interactive text output and generate button
+ */
 const text = document.getElementById("text");
 const button = document.getElementById("gen-btn");
 
-const options = {
-  name,
-  age: ["child", "adolescent", "adult", "elderly"],
-  species: ["earth pony", "unicorn", "pegasus", "bat pony", "kirin", "seapony", "hippogriff", "zebra", "griffon", "changeling", "reformed changeling", "draconequus", "crystal pony"],
-  //colorPalette: a WIP feature
-  personalityTrait,
-  misc: [""],
-  height: [""],
-  bodyType: [""],
-  cutieMark:[""],
-  hair: {
-    hairLength: [""],
-    hairTexture: [""]
-  }
-};
-
-const createRandomCreature = () => {
-  return {
-    name: options.name.selectUnique(2).join(" "),
-    age: options.age[getRandomInt(options.age.length)],
-    species: options.species[getRandomInt(options.species.length)],
-    personalityTrait: options.personalityTrait[getRandomInt(options.personalityTrait.length)]
-  };
-};
-
+/**
+ * Generate button event listener
+ */
 button.addEventListener("click", () => {
-  const creature = createRandomCreature();
+  const creature = generateCreature();
   text.innerText = `Your new character is called ${creature.name}. They are ${
     ["a", "e", "i", "o", "u"].includes(creature.age[0]) ? "an" : "a"
-  } ${creature.age} ${creature.species}. Their personality can be described as ${creature.personalityTrait}.`;
+  } ${creature.age} ${creature.species}. Their personality can be described as ${creature.trait}.`;
 });
